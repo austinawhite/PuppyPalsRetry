88bf8d15f495ddc547d229deb94294b80032b517
Austina White <austinawhite@gmail.com>
2025-03-29 12:16:59 -0400

    updated page

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,47 @@
 import { useState } from 'react'
 import './App.css'
+import {puppyList} from './data.js'
 
 function App() {
- 
+
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null)
+
+  console.log("puppyList: ", puppyList);
+
+  function handleClick() {
+    // some logic here
+  }
+
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId)
+
 
   return (
-    <>
-      <div>
-    
-      </div>
-      
-    </>
-  )
+    <div className="App">
+      {puppies.map((puppy) => {
+        return (
+          <p key={puppy.id} onClick={()=> setFeatPupId(puppy.id)}>
+          {puppy.name}
+          </p>
+        );
+      })}
+
+{featPupId && (
+  <div>
+    <br></br>
+    <br></br>
+    <h2>{featuredPup.name}</h2>
+    <ul>
+      <li>Age: {featuredPup.age}</li>
+      <li>Email: {featuredPup.email}</li>
+    </ul>
+  </div>
+)}
+
+    </div>
+  );
 }
+
+
 
 export default App
